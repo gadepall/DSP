c6859f9bc0cf64850aeab3477d0572fa061f059e
gadepall <gadepall@gmail.com>
2018-11-03 19:32:42 +0530

    Add existing files

--- fourier/transform/codes/lpf.py
+++ fourier/transform/codes/lpf.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import subprocess
+import shlex
 
 #Filter Characteristics
 
@@ -7,7 +9,7 @@ R = 5*1e2; #1K ohm resistance
 C = 10e-6;#10 uF capacitance
 
 #Plotting the filter amplitude response
-f_0 = 50
+f_0 = 50.0
 f = np.linspace(-3*f_0,3*f_0,1e2)
 s = 1j*2*np.pi*f
 
@@ -20,7 +22,9 @@ plt.xlabel('$f$ (Hz)')
 plt.ylabel('$H(f)$')
 #Save figure
 #plt.savefig('../figs/lpf.eps')
-plt.show()
+plt.savefig('lpf.pdf')
+subprocess.run(shlex.split("termux-open lpf.pdf"))
+#plt.show()
 
 
 
