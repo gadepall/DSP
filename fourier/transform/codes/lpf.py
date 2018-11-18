f985675054c8a79af3ff6f23a9013b3669f7f482
gadepall <gadepall@gmail.com>
2018-11-18 17:06:59 +0530

    Add existing files

--- fourier/transform/codes/lpf.py
+++ fourier/transform/codes/lpf.py
@@ -3,6 +3,14 @@ import matplotlib.pyplot as plt
 import subprocess
 import shlex
 
+#RC filter frequency response
+def H(f):
+	s = 1j*2*np.pi*f
+	den = np.polyval([1,1],s*C*R)
+	H = 1/den
+	return H
+
+
 #Filter Characteristics
 
 R = 5*1e2; #500 ohm resistance
@@ -11,12 +19,8 @@ C = 10e-6;#10 uF capacitance
 #Plotting the filter amplitude response
 f_0 = 50.0
 f = np.linspace(-3*f_0,3*f_0,1e2)
-s = 1j*2*np.pi*f
 
-den = np.polyval([1,1],s*C*R);
-H = 1/den;
-
-plt.plot(f,abs(H))
+plt.plot(f,abs(H(f)))
 plt.grid()# minor
 plt.xlabel('$f$ (Hz)')
 plt.ylabel('$H(f)$')
